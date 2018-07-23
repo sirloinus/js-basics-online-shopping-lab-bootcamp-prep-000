d7a0b412b97898ebc809e140163de4f561094313
Amalie S <amaliesmidth@gmail.com>
2018-07-23 15:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function placeOrder(cardNumber) {
   } else {
     let totalCostToCharge = total()
     cart.length = 0
-    return `Your  total cost is ${total}, which will be charged to the card ${cardNumber}.`
+    return `Your  total cost is ${totalCostToCharge}, which will be charged to the card ${cardNumber}.`
   }
 }
 
