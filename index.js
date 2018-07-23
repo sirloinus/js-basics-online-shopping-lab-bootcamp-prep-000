bb373362168462811f91ec0ec40237c4015b674c
Amalie S <amaliesmidth@gmail.com>
2018-07-23 15:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,8 +91,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    return ''
+    return "Sorry, we don't have a credit card on file for you."
   }
+  if ()
   
 }
 
