1004118ddd00d82f89381d140f672a2fb6d19467
Amalie S <amaliesmidth@gmail.com>
2018-07-23 15:30:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,10 +94,9 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   if (cardNumber) {
-    let basket = 
     cart.length = 0
+    return `Your  total cost is ${total}, which will be charged to the card ${cardNumber}.`
   }
-  
 }
 
 
