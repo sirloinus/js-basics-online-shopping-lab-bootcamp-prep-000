44ee6cbcbe30d3566551ddb61290b19a5a698b4d
Amalie S <amaliesmidth@gmail.com>
2018-07-23 15:26:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -93,7 +93,9 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
-  if ()
+  if (cardNumber) {
+    
+  }
   
 }
 
